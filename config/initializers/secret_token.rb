d6d5adafdb9da90198ba4052cef0be077148eb68
Daniel Willems <dmkwillems@gmail.com>
2013-08-14 19:52:26 -0700

    For real this time.  Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthCarrot::Application.config.secret_token = '0436e2dcc7def84a3260754d028da98fdf6c6abaaad080acaf341f9969ffbb1e48235b4b9a894aa5f0344b47622a489bbaf886c2f60d2eb156f6cf1b5ba65453'
